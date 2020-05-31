2a19fb6967162294c6c2233bf400be68ddaa2fe8
miracle173 <guenter.nowak@gmx.at>
2020-05-31 06:24:08 +0000

    compose imagelists of different length, result is normed tuple

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
 from itertools import chain
 
 def compose(imagelist1,imagelist2):
-    if len(imagelist1)!=len(imagelist2):
-        raise(Exception("invalid sizes"))
-    result=[0]*len(imagelist1)
-    for i in range(len(imagelist1)):
-        result[i]=imagelist2[imagelist1[i]-1]
-    return(result)
+    # stretch smaller imagelist
+    if len(imagelist1)==len(imagelist2):
+        il1=imagelist1
+        il2=imagelist2
+    elif len(imagelist1)<len(imagelist2):
+        il1=[imagelist1[i] if i<len(imagelist1) else i for i in range(len(imagelist2))]
+        il2=imagelist2
+    elif len(imagelist2)<len(imagelist1):
+        il1=imagelist1
+        il2=[imagelist2[i] if i<len(imagelist2) else i for i in range(len(imagelist1))]
+    # compose lists
+    result=[0]*len(il1)
+    for i in range(len(il1)):
+        result[i]=il2[il1[i]-1]
+    plen=len(result)
+    for i in range(len(result)-1,-1,-1):
+        if result[i]==i+1:
+            plen=i
+        else:
+            break
+    return(tuple(result[:plen]))
 
 def is_imagelist(imagelist):
     return(sorted(imagelist)==list(range(1,len(imagelist)+1)))
@@ -72,16 +87,27 @@ def dict_from_imageform(imagelist):
 
 
 class permutation(object):
-    def __init__(self):
+    def __init__(self,data):
         if isinstance(data,list):
             pass
-        else isinstance(data, permutation):
+        elif isinstance(data, permutation):
             pass
 
 p=[5,7,2,1,3,4,6]
-p=[1,2,3,4,5,6,7]
+#p=[1,2,3,4,5,6,7]
 print(p) 
 print(cycleform(p))
 
 c=[[2,3,4],[6,7]]
 print(imageform(c))
+
+p1=[2,1,3,4]
+p2=[4,3,2,1]
+print()
+print(p1)
+print(p2)
+print(compose(p1,p2))
+print(compose(p2,p1))
+print(compose(p1,p1))
+print(compose(p,p2))
+print(compose(p2,p))
